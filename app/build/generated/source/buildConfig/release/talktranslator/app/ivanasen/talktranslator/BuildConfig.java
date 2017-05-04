7af7601be2decfb360983d73d4d78e93834fa5f3
vankata9907 <vankata9907@gmail.com>
2017-05-05 01:17:30 +0300

    New Debug APK

---
+++ app/build/generated/source/buildConfig/release/talktranslator/app/ivanasen/talktranslator/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package talktranslator.app.ivanasen.talktranslator;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "talktranslator.app.ivanasen.talktranslator";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 3;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: release
+  public static final String YANDEX_TRANSLATE_API_KEY = "trnsl.1.1.20170107T161029Z.5f57d865274bc32c.ff7f171f966fa481573995abdaca9b479e8b144d";
+}
